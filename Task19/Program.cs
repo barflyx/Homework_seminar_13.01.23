78157431b10a8b2c648ffd18b6488136d7ed3768
Ilia <89165005679@ya.ru>
2023-02-09 21:13:38 +0400

    Added Task 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 19
+
+Напишите программу, которая принимает на вход пятизначное число и проверяет,
+является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
+
+
+bool GetAnswer(int num) 
+{
+return num / 10000 == num % 10 && num / 1000 % 10 == num / 10 % 10;
+}
+
+Console.Write("Введите пятизначное положительное натуральное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+while (Number < 0 || Number > 99999 || Number < 10000)
+{
+    Console.Write("Вы ввели неправильное число. Введите пятизначное положительное натуральное число: ");
+    Number = Convert.ToInt32(Console.ReadLine());
+}
+
+bool result = GetAnswer(Number);
+Console.WriteLine(result ? $"Число {Number} палиндром." : $"Число {Number} не палиндром.");
+
