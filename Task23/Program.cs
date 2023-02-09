ab115b635313384550119ddaecba115ee09dcd8d
Ilia <89165005679@ya.ru>
2023-02-09 21:32:30 +0400

    Solution Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 23
+Напишите программу, 
+которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+void GetCubesTable(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count,5}{Math.Pow(count, 3),5}");
+        count++;
+    }
+
+}
+
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+while (Number < 1)
+{
+    Console.Write("Вы ввели неправильное число. Введите положительное натуральное число: ");
+    Number = Convert.ToInt32(Console.ReadLine());
+}
+
+GetCubesTable(Number);
