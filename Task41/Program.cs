0605ee537f1d9847539e2bfdf4359e50dab3958a
Ilia <89165005679@ya.ru>
2023-02-21 10:54:46 +0400

    Solution Task41_43

---
+++ Task41/Program.cs
@@ -0,0 +1,49 @@
+﻿/* 
+Задача 41: 
+Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+
+int[] GenerateArray(int length)
+{
+ int[] array = new int[length];
+ for (int i = 0; i < length; i++)
+     {
+ Console.Write($"Введите {i + 1} число: ");
+ array[i] = Convert.ToInt32(Console.ReadLine());
+     }
+ return array;
+}
+
+string PrintArray(int[] arr)
+{
+ string str = "[";
+ for (int i = 0; i < arr.Length; i++)
+     {
+ if (str == "[") str += arr[i];
+ else str += $", {arr[i]}";
+     }
+ str += "]";
+ return str;
+
+ }
+
+ int GetCountPositiveNumbers(int[] array)
+ {
+ int count = 0;
+for (int i = 0; i < array.Length; i++)
+     {
+ if (array[i] > 0) count++;
+     }
+ return count;
+ }
+
+Console.Write("Сколько числел вы будете вводить? - ");
+int userSizeArray = Convert.ToInt32(Console.ReadLine());
+
+int[] arrayNumbers = GenerateArray(userSizeArray);
+
+int countPositiveNumbers = GetCountPositiveNumbers(arrayNumbers);
+Console.WriteLine($"В массиве {PrintArray(arrayNumbers)} {countPositiveNumbers} положительных чисел(ла).");
---
+++ Task43/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задача 43: 
+Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double GetCoordinatesX(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    return Math.Round(x, 1);
+}
+
+double GetCoordinatesY(double b1, double k1, double b2, double k2, double x)
+{
+    double y = (x * (k1 + k2) + b1 + b2) / 2;
+    return Math.Round(y, 1);
+}
+
+Console.WriteLine("Введите координаты прямых."); //везде тип double так как пользователь может ввести вещественное число, а условие задачи не исключает это.
+
+Console.Write("Введите b1: ");
+double userB1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите K1: ");
+double userK1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double userB2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double userK2 = Convert.ToDouble(Console.ReadLine());
+
+if (userB1 == userB2 && userK1 == userK2) Console.WriteLine($"Прямые совпадают!");
+else if (userK1 == userK2) Console.WriteLine($"Прямые паралельны!");
+else
+{
+    double coordinatesX = GetCoordinatesX(userB1, userK1, userB2, userK2);
+    double coordinatesY = GetCoordinatesY(userB1, userK1, userB2, userK2, coordinatesX);
+
+    Console.WriteLine($"Координаты точки пересечения ({coordinatesX}, {coordinatesY})");
+}
+
